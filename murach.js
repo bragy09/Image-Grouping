95e8b9cd217747d01e085d94da0c78e9d52d16fc
bragy09 <50281853+bragy09@users.noreply.github.com>
2020-03-23 10:03:18 -0500

    Add files via upload

---
+++ murach.js
@@ -0,0 +1,59 @@
+﻿window.onload = function () {
+    var listNode = document.getElementById("murach");
+    var h2Elements = listNode.getElementsByTagName("h2");
+    var h2Node;
+    for (var i = 0; i < h2Elements.length; i++) {
+        h2Node = h2Elements[i];
+
+        // Attach event handler
+        h2Node.onclick = function () {
+
+            var h2 = this;  // h2 is the current h2Node object
+
+            if (h2.getAttribute("class") == "plus")
+                h2.setAttribute("class", "minus");
+            else
+                h2.setAttribute("class", "plus");
+
+            if (h2.nextElementSibling.getAttribute("class") == "closed")
+                h2.nextElementSibling.setAttribute("class", "open");
+            else
+                h2.nextElementSibling.setAttribute("class", "closed");
+
+            document.getElementById("image").setAttribute('style', 'display:none;');
+        }
+    }
+
+    // set up the image display
+    var imageNode = document.getElementById("image");
+
+    var imageLinks = listNode.getElementsByTagName("a");
+
+    // Process image links
+    var i, linkNode, image;
+    for (i = 0; i < imageLinks.length; i++) {
+        linkNode = imageLinks[i];
+
+        // Attach event handler
+        linkNode.onclick = function (evt) {
+            var link = this;          // link is the linkNode
+            imageNode.src = link.getAttribute("href");
+            // needed for IE to display the image
+            imageNode.setAttribute("style", "display:block;");
+
+            // Cancel the default action of the event
+            if (!evt) { evt = window.event; }
+            if (evt.preventDefault) {
+                evt.preventDefault();          // DOM compliant code
+            }
+            else {
+                evt.returnValue = false;
+            }
+        }
+
+        ///////////////////////////
+        // Preload image
+        image = new Image();
+        image.src = linkNode.getAttribute("href");
+    }
+}
